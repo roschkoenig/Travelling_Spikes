9e4d8a24d371f3c59f179b1bce349f8b2d5b049a
Stasa Tumpa <stasa.tumpa@icloud.com>
2018-06-09 14:21:09 +0100

    added housekeeping to ignored files

--- ts_housekeeping.m
+++ ts_housekeeping.m
@@ -1,6 +1,14 @@
 function D = ts_housekeeping
 fs          = filesep;
+<<<<<<< HEAD
 D.Fbase     = '/Users/stasa.tumpa/Documents/GitHub';
+=======
+if strcmp(computer, 'PCWIN64'); 
+    D.Fbase     = 'C:\Users\rrosch\Dropbox\Research\Friston Lab\1805 Travelling Spikes';
+else
+    D.Fbase     = '/Users/roschkoenig/Dropbox/Research/Friston Lab/1805 Travelling Spikes';
+end
+>>>>>>> 65935f628ef17f84bb4d6db0c184f1d1fddc654d
 D.Fdata     = [D.Fbase fs 'Data'];
 D.Fscripts  = [D.Fbase fs 'Scripts'];
 addpath(genpath(D.Fbase));
