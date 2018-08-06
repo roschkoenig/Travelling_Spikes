8a28d397257b0fdf9b9cb975624c3aaf995ce16e
Richard Rosch <roschkoenig@gmail.com>
2018-08-06 16:56:10 +0100

    Updated DCM analysis (to do single spikes)

--- ts_dcm_specify.m
+++ ts_dcm_specify.m
@@ -26,7 +26,7 @@ dists           = str2double(chanlabels(MEEG));
 [loc(2).dist, loc(2).id]	= min(abs(dists));      loc(2).name = 'cr';
 [loc(3).dist, loc(3).id]	= max(dists);           loc(3).name = 'gm';
 
-for l = 1:length(loc)    
+% for l = 1:length(loc)    
     
 % Set up folders, set up shop
 %--------------------------------------------------------------------------
@@ -36,18 +36,18 @@ try, mkdir([D.Fanalysis fs 'DCM']); end
 dotpos = find(meegfiles{m} == '.');
 seppos = find(meegfiles{m} == '_');
 el              = meegfiles{m}(seppos(end)+1:dotpos(end)-1);
-DCM.name        = [D.Fanalysis fs 'DCM' fs sub '_' el '_' loc(l).name];
+DCM.name        = [D.Fanalysis fs 'DCM' fs sub '_' el];
 
 % Parameters and options used for setting up model
 %--------------------------------------------------------------------------
 DCM.options.trials      = 1:length(conds);
 DCM.options.analysis    = 'ERP';
-DCM.options.model       = 'ERP';
+DCM.options.model       = 'CMC';
 DCM.options.spatial     = 'LFP';
-DCM.options.Tdcm        = [-150 350];
-DCM.options.onset       = -50;
+DCM.options.Tdcm        = [-200 500];
+DCM.options.onset       = -100;
 DCM.options.dur         = 20;
-DCM.options.Nmodes      = 3;
+DCM.options.Nmodes      = 6;
 DCM.options.h           = 1;
 DCM.options.han         = 1;
 DCM.options.D           = 1;
@@ -66,18 +66,22 @@ DCM.A{3} = zeros(Nareas);   % Lateral
 for a = 1:Nareas-1
 	DCM.A{1}(a+1, a) = 1;
     DCM.A{2}(a, a+1) = 1;
+    DCM.A{3}(a,a)    = 1;
 end
+DCM.A{3}(a+1,a+1)    = 1;
 
 for i = 1:length(conds)-1
     DCM.B{i} = zeros(Nareas) + DCM.A{1} + DCM.A{2} + DCM.A{3};
-    for b = 1:length(DCM.B{i})
-        DCM.B{i}(b,b) = 1;
-    end
+%     for b = 1:length(DCM.B{i})
+%         DCM.B{i}(b,b) = 1;
+%     end
 end
 
 DCM.C = zeros(Nareas,1);
-DCM.C(loc(l).id) = 1;
-disp(['Giving the model input at ' loc(l).name])
+for l = 1:length(loc)
+    DCM.C(loc(l).id) = 1;
+end
+% disp(['Giving the model input at ' loc(l).name])
 
 % Between trial effects
 %--------------------------------------------------------------------------
@@ -87,8 +91,14 @@ for i = 1:length(conds)-1
     DCM.xU.name{i}    = ['Transition to ' num2str(i+1)];
 end
 
+% Update priors
+%--------------------------------------------------------------------------
+[pE,pC] = spm_dcm_neural_priors(DCM.A,DCM.B,DCM.C,DCM.options.model);
+pE.T    = [1 -.5 1 -.5];
+DCM.M.pE  = pE;
+DCM.M.pC  = pC;
+
 DCM = spm_dcm_erp(DCM);
 
-end
 end
     
